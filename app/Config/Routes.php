58a28dd64b5a8a416a2c8348439ab509d0124d82
ChiristianK <91868266+ChiristianK@users.noreply.github.com>
2024-06-03 08:27:20 +0700

    Update Routes.php
    Adding Backend API for Acces Control

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -21,3 +21,60 @@ $routes->get('/edit_berita/(:any)', 'Berita::edit_berita/$1');
 $routes->get('/delete_berita/(:any)', 'Berita::delete_berita/$1');
 $routes->post('/proses_add_berita', 'Berita::proses_add_berita');
 $routes->post('/proses_edit_berita', 'Berita::proses_edit_berita');
+
+service('auth')->routes($routes);
+
+// API Routes
+$routes->group("api", ["namespace" => "App\Controllers\Api"], function ($routes) {
+
+  // Get
+  $routes->get("invalid-access", "AuthController::accessDenied");
+  // Post
+  $routes->post("register", "AuthController::register");
+  // Post
+  $routes->post("login", "AuthController::login");
+  // Get
+  $routes->get("logout", "AuthController::logout", ["filter" => "auth"]);
+
+
+  $routes->group("administrator", ["namespace" => "App\Controllers\Api"], function($routes) {
+    // Post
+    $routes->post("register", "AdministratorController::register");
+  });
+
+  
+  $routes->group("manajer", ["namespace" => "App\Controllers\Api"], function($routes) {
+    // Post
+    $routes->post("register", "ManajerController::register", ["filter" => "auth"]);
+    // Get
+    $routes->get("list", "ManajerController::list", ["filter" => "auth"]);
+    // Delete
+    $routes->delete("delete/(:num)", "ManajerController::deleteManager/$1", ["filter" => "auth"]);
+  });
+
+  
+  $routes->group("supplier", ["namespace" => "App\Controllers\Api"], function($routes) {
+    // Get
+    $routes->get("profile", "SupplierController::profile", ["filter" => "auth"]);
+    // Get
+    $routes->get("list", "SupplierController::list", ["filter" => "auth"]);
+    // Get
+    $routes->get("single/(:num)", "SupplierController::single/$1", ["filter" => "auth"]);
+    // Put
+    $routes->put("update", "SupplierController::updateData", ["filter" => "auth"]);
+    // Delete
+    $routes->delete("delete/(:num)", "SupplierController::deleteSupplier/$1", ["filter" => "auth"]);
+  });
+
+
+  $routes->group("news", ["namespace" => "App\Controllers\Api"], function($routes) {
+    // Post
+    $routes->post("add", "NewsController::addNews", ["filter" => "auth"]);
+    // Get
+    $routes->get("list", "NewsController::listNews");
+    // Put
+    $routes->put("update/(:num)", "NewsController::updateNews/$1", ["filter" => "auth"]);
+    // Delete
+    $routes->delete("delete/(:num)", "NewsController::deleteNews/$1", ["filter" => "auth"]);
+  });
+});
